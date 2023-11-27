6449116b05baba249b4c12db25d14ccf0db083c6
YuriySW <yuriy.s@my.com>
2023-11-28 02:25:35 +0500

    lesson 4.3 HW 1.2

--- newFolder/tasksHard.js
+++ newFolder/tasksHard.js
@@ -16,14 +16,20 @@ if (isNaN(income)) {
 
 {
   const income = +prompt('Введите ваш доход:');
+  let taxAfter = 0;
+  let taxUpTo = 0;
 
   if (isNaN(income)) {
     console.log('Введите сумму в цифрах');
   } else if (income <= 15000) {
     console.log(`Сумма вашего налога ${income * 0.13}`);
   } else if (income > 15000 && income <= 50000) {
-    console.log(`Сумма вашего налога ${(income - 15000) * 0.2}`);
+    taxAfter = income - 15000;
+    taxUpTo = income - taxAfter;
+    console.log(`Сумма вашего налога ${taxAfter * 0.2 + taxUpTo * 0.13}`);
   } else if (income > 50000) {
-    console.log(`Сумма вашего налога ${(income - 50000) * 0.3}`);
+    taxAfter = income - 50000;
+    taxUpTo = income - taxAfter;
+    console.log(`Сумма вашего налога ${taxAfter * 0.3 + taxUpTo * 0.2}`);
   }
 }
