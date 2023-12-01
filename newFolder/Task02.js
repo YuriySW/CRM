a7b6148a3ac3ecc956924f89238722ee210565e4
YuriySW <yuriy.s@my.com>
2023-12-01 22:16:30 +0500

    modul 4

--- newFolder/Task02.js
+++
@@ -1,8 +0,0 @@
-const modificationString = (words) => {
-  let toWords = words.split(' ');
-  const modWord = toWords[0][0].toUpperCase() + toWords[0].substring(1);
-  const modWords = modWord.concat(' ', toWords[1].toLowerCase());
-  return console.log(modWords);
-};
-
-modificationString('привет Мир');
