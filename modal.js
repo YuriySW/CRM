5cd2615f42ccebee467c823aa2d5d20ea190ed87
YuriySW <yuriy.s@my.com>
2024-03-14 19:56:01 +0500

    const for modal

---
+++ modal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const overlayDisplayNone = document.querySelector('.overlay ');
+const overlayDisplayFlex = document.querySelector('.overlay_flex ');
+const titleModal = document.querySelector('.popup__title');
+const formModal = document.querySelector('.form');
+const checkbox = document.querySelector('.form__checkbox');
+const checkboxInput = document.querySelector('.form__input');
+const totalAmount = document.querySelector('.amount-money');
