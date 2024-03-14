f2d2b922cf45415887cf1dc67507f67cab963fd2
YuriySW <yuriy.s@my.com>
2024-03-15 01:04:00 +0500

    lesson 5.2 hw 1.0

--- modal.js
+++ modal.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const overlayDisplayNone = document.querySelector('.overlay ');
-const overlayDisplayFlex = document.querySelector('.overlay_flex ');
 const titleModal = document.querySelector('.popup__title');
 const formModal = document.querySelector('.form');
 const checkbox = document.querySelector('.form__checkbox');
-const checkboxInput = document.querySelector('.form__input');
+const discount = document.querySelector('.discount');
 const totalAmount = document.querySelector('.amount-money');
