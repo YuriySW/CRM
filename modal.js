0758b284fa45dc1270e8950d2fa196434c505bf9
YuriySW <yuriy.s@my.com>
2024-03-17 15:09:57 +0500

    lesson 5.2 hw 1.2

--- modal.js
+++ modal.js
@@ -2,6 +2,6 @@
 
 const titleModal = document.querySelector('.popup__title');
 const formModal = document.querySelector('.form');
-const checkbox = document.querySelector('.form__checkbox');
+const checkboxDiscount = document.querySelector('.form__checkbox');
 const discount = document.querySelector('.discount');
 const totalAmount = document.querySelector('.amount-money');
