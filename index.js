c3fa5c4c32c3863afa53cd7677ca5ad0621445fd
YuriySW <yuriy.s@my.com>
2024-05-02 12:41:47 +0500

    lesson 5.10 hw 1.2

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const nameOfProduct = 'Macbook air';
-const quantityOfGoods = 367;
-const productCategory = 'Laptop';
-const priceOfProduct = 1000;
-
-console.log(`Наименование товара: ${nameOfProduct}.`);
-console.log(`Общая сумма товара: ${priceOfProduct * quantityOfGoods} рублей.`);
